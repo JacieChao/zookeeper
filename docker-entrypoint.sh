dc1fcab69dd43d1789c91706ea5d0f0c3a29f4b8
wangc3 <wangc3@neunn.com>
2017-10-31 15:40:09 +0800

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,8 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
     echo "tickTime=$ZOO_TICK_TIME" >> "$CONFIG"
     echo "initLimit=$ZOO_INIT_LIMIT" >> "$CONFIG"
     echo "syncLimit=$ZOO_SYNC_LIMIT" >> "$CONFIG"
+	
+	echo "aaaa=$(hostname | awk -F'-' '{$NF="";print}')" >> "$CONFIG"
 
     # for server in $ZOO_SERVERS; do
     #     echo "$server" >> "$CONFIG"
@@ -27,8 +29,8 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
     	if [ "$[count-1]" -eq "$(hostname | awk -F'-' '{print $2}')" ];then
     		echo "server.$count=0.0.0.0:2888:3888:participant" >> "$CONFIG";
     	else
-        	#echo "server.$count=zoo-$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
-			echo "server.$count=$(hostname | awk -F'-' '{NF-=1;print}')-$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
+        	echo "server.$count=zoo-$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
+			#echo "server.$count=$(hostname | awk -F'-' '{$NF="";print}')$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
         fi
     done
 fi
