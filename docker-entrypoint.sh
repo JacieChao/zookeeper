f278e8f875adbdb3a6b5580a2567d698d5f4b96a
wangc3 <wangc3@neunn.com>
2017-11-01 09:29:47 +0800

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,17 +29,22 @@ if [ ! -f "$ZOO_CONF_DIR/zoo.cfg" ]; then
     	else
         	#echo "server.$count=zoo-$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
 			#echo "hua-nong-jing-chao"|awk -F '-' '{ for(i=1;i<NF;i++) {str=str$i"-"}}{print str}'
-			echo "server.$count=$(hostname | awk -F '-' '{ for(i=1;i<NF;i++) {str=str$i"-"}}{print str}')$[count-1].$DNS:2888:3888:participant"
-			echo "server.$count=$(hostname | awk -F '-' '{ for(i=1;i<NF;i++) {str=str$i"-"}}{print str}')$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
+			echo "server.$count=$(hostname | awk -F '-' '{ for(i=1;i<NF;i++) {str=str$i$ZOO_SPLIT}}{print str}')$[count-1].$DNS:2888:3888:participant"
+			echo "server.$count=$(hostname | awk -F '-' '{ for(i=1;i<NF;i++) {str=str$i$ZOO_SPLIT}}{print str}')$[count-1].$DNS:2888:3888:participant" >> "$CONFIG";
         fi
     done
 fi
 
 # Write myid only if it doesn't exist
+echo "aaaaaaaa"
 if [ ! -f "$ZOO_DATA_DIR/myid" ]; then
 #    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+	echo "bbbbbbbbbb"
     zooId=$(hostname | awk -F'-' '{print $NF}')
+	echo "cccccccccccccc"
     echo "$[zooId+1]" > "$ZOO_DATA_DIR/myid"
+	echo "dddddddddd"
 fi
 
+echo "eeeeeeeeeeee"
 exec "$@"
